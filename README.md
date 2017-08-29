21bb06c8eccd9946c3fd5a79970818842effe367
Risto Stevcev <risto1@gmail.com>
2017-08-29 11:04:50 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# primary-clipboard
+
+[![Latest release](http://img.shields.io/github/release/Risto-Stevcev/primary-clipboard.svg)](https://github.com/Risto-Stevcev/primary-clipboard/releases)
+
+A layer that sets up copy and paste for the PRIMARY clipboard in spacemacs windowless mode (`-nw`)
+
+
+## Installation
+
+- Clone the repo into your `~/.emacs.d/private/` folder, so you end up with `~/.emacs.d/private/primary-clipboard/`
+- Add `primary-clipboard` to your list of layers in the `dotspacemacs-configuration-layers` section of your `.spacemacs` file
---
+++ packages.el
@@ -0,0 +1,44 @@
+;;; packages.el --- primary-clipboard layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
+;;
+;; Author:  <risto>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;;; Commentary:
+
+;; See the Spacemacs documentation and FAQs for instructions on how to implement
+;; a new layer:
+;;
+;;   SPC h SPC layers RET
+;;
+;;
+;; Briefly, each package to be installed or configured by this layer should be
+;; added to `primary-clipboard-packages'. Then, for each package PACKAGE:
+;;
+;; - If PACKAGE is not referenced by any other Spacemacs layer, define a
+;;   function `primary-clipboard/init-PACKAGE' to load and initialize the package.
+
+;; - Otherwise, PACKAGE is already referenced by another Spacemacs layer, so
+;;   define the functions `primary-clipboard/pre-init-PACKAGE' and/or
+;;   `primary-clipboard/post-init-PACKAGE' to customize the package as it is loaded.
+
+;;; Code:
+
+(defconst primary-clipboard-packages
+  '((xclip :location elpa)))
+
+(defun primary-clipboard/init-xclip ()
+  ;; Sets ups copy/paste from PRIMARY clipboard
+  (require 'xclip)
+  (xclip-mode 1)
+  (setq x-select-enable-clipboard t)
+  (setq interprogram-paste-function 'x-cut-buffer-or-selection-value)
+
+  ;; To get the middle mouse button to paste from the PRIMARY clipboard, use:
+  ;; (xterm-mouse-mode -1)
+  )
